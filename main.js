128bae45cf280f98702f054f559ede9498371d43
Brennan Fairfull <brennan.fairfull@gmail.com>
2022-06-18 15:56:23 +1000

    generalise callback functionality

--- main.js
+++ main.js
@@ -5,6 +5,12 @@ const board = new Board({
   port: "COM3"
 });
 
+function onPotChange(potentiometer) {
+  console.log(`Sensor: ${potentiometer.sensorName}`);
+  console.log("  value  : ", potentiometer.value);
+  console.log("-----------------");
+}
+
 board.on("ready", () => {  
-  new Potentiometer("A0")
+  new Potentiometer("A0", onPotChange)
 });
--- potentiometer.js
+++ potentiometer.js
@@ -1,22 +1,23 @@
 const { Sensor } = require("johnny-five");
 
 class Potentiometer {
-  constructor(sensorName) {
+  constructor(sensorName, onChange) {
     this.sensorName = sensorName;
     this.sensor = new Sensor(sensorName);
     this.sensorValue = 0;
-    this.sensorValueNormalised = 0;
+    this.onChange = onChange
 
     this.setupListener()
   }
 
   setupListener() {
     this.sensor.on("change", () => {
-      const { value } = this.sensor;
-      console.log(`Sensor: ${this.sensorName}`);
-      console.log("  value  : ", value);
-      console.log("-----------------");
+      this.onChange(this)
     });
+  }
+
+  get value() {
+    return this.sensor.value
   }
 }
 
