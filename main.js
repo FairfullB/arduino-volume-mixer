629cea1edb5499c323f0a5cc9860faf4a3398be7
Brennan Fairfull <brennan.fairfull@gmail.com>
2022-06-18 20:08:40 +1000

    add rocket league and dota volumne dials

--- main.js
+++ main.js
@@ -8,4 +8,6 @@ const board = new Board({
 board.on("ready", () => {  
   new VolumneController("A0", "spotify");
   new VolumneController("A1", "discord");
+  new VolumneController("A2", "rl");
+  new VolumneController("A3", "dota");
 });
